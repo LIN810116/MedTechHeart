f7942068d6c227bcb2b9107a4107e7f2ad0f9754
Jenny Wang <zwan145@aucklanduni.ac.nz>
2017-11-16 15:56:35 +1300

    Label memory tweak
    Labels were using way too much memory. Scaling that down fixes large memory issues.

--- js/Add3DLabel.js
+++ js/Add3DLabel.js
@@ -1,36 +1,36 @@
 var createFont = function(text, x, y, z, scaling) {
 	//create an element to render the font on
 	var bitmap = document.createElement('canvas');
-	bitmap.width = 8192;
-	bitmap.height = 8192;
+	bitmap.width = 512;
+	bitmap.height = 512;
 	var g = bitmap.getContext('2d');
 	//draw boundary for debugging
 	//g.strokeStyle = '#ff0000';
 	//g.strokeRect(0, 0, 560, 560);
 
-	g.textBaseline = "alphabetic"; 
+	g.textBaseline = "alphabetic";
 
-	var metrics = g.measureText( text ); 
-	var textWidth = metrics.width; 
-	
+	var metrics = g.measureText( text );
+	var textWidth = metrics.width;
+
 	//draw the font on the element
 	g.fillStyle = 'rgb(255,255,255)';
-	g.textAlign = "centre"; 
-	g.font = '480px Helvetica';
-    g.fillText(text, 4096,4096);
+	g.textAlign = "centre";
+	g.font = '30px Helvetica';
+    g.fillText(text, 256,256);
 	g.strokeStyle = 'rgba(255,255,255)';
-	g.strokeText(text,4096,4096);
-	
+	g.strokeText(text,256,256);
+
 	//create a texture with the element and put it on threejs object
 	var texture = new THREE.Texture(bitmap);
 	texture.needsUpdate = true;
-	var spriteMaterial = new THREE.SpriteMaterial( {  map: texture, color: '#ffffff'} ); 
-	var sprite = new THREE.Sprite( spriteMaterial );   
+	var spriteMaterial = new THREE.SpriteMaterial( {  map: texture, color: '#ffffff'} );
+	var sprite = new THREE.Sprite( spriteMaterial );
 
 	//scale up the texture
 	sprite.scale.set( scaling, scaling, 1.0 );
 
-	// now set the position at the correct 3D space	
+	// now set the position at the correct 3D space
 	sprite.position.set(x, y, z);
 	return sprite;
 }
